460cc8ad55fddc2caaad1af38c5a9f65fc5581af
emamonjes <136394336+emamonjes@users.noreply.github.com>
2024-03-29 19:54:51 -0300

    a

--- romeco/src/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Importamos el componente principal de la aplicación
-
-ReactDOM.render(<App />, document.getElementById('root')); // Renderizamos la aplicación en el elemento con el ID 'root'
