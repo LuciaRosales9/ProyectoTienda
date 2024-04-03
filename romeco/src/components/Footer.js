10910a245c0d857ee3ba76744eb716c54fbaa629
emamonjes <136394336+emamonjes@users.noreply.github.com>
2024-04-03 18:46:00 -0300

    a

--- romeco/src/components/Footer.js
+++ romeco/src/components/Footer.js
@@ -1,10 +1,10 @@
-import '../styles/Footer.css'; // Importa el archivo de estilos CSS,  <a href="#titulo">Ir al comienzo</a> 
+
 
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer >
       <section>
             <a href="mailto:romecojewelry@gmail.com">Contáctame aquí</a>
         </section>
