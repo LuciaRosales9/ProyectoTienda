da0f3699ada83525977ecee263b5b5b2bbbfa545
AbrilConrero <abrilconrero@gmail.com>
2024-04-05 19:10:11 -0300

    Cambios en Romeco
    Se cambió la fuente
    Se creó un archivo galleryRomeco.js con una galería para los locales, con un link a maps
    Se hicieron cambios estéticos

---
+++ romeco/src/components/galleryRomeco.js
@@ -0,0 +1,26 @@
+import '../styles/Gallery.css'
+
+export const GalleryRomeco = () => {
+  return (
+        <section id="gallery">
+    <figure id="1">
+        <a href="https://www.google.com/maps/place/Aguas+De+Villa+María/@-32.4131603,-63.2487457,17z/data=!3m1!4b1!4m6!3m5!1s0x95cc42e0e207eb43:0x56897eb7fb5b663a!8m2!3d-32.4131649!4d-63.2461708!16s%2Fg%2F11h75pt_yk?entry=ttu" target="_blank" rel="noopener noreferrer">
+            <img src="/images/_06b00927-c3eb-4807-bfc1-549bf1cf9a9e.jpg" title="Local Romeco 1"></img>
+        </a>
+    </figure> 
+
+    <figure id="2">
+        <a href="https://www.google.com/maps/place/Aguas+De+Villa+María/@-32.4131603,-63.2487457,17z/data=!3m1!4b1!4m6!3m5!1s0x95cc42e0e207eb43:0x56897eb7fb5b663a!8m2!3d-32.4131649!4d-63.2461708!16s%2Fg%2F11h75pt_yk?entry=ttu" target="_blank" rel="noopener noreferrer">   
+            <img src="/images/OIG1.jpg" width="100" alt="Local Romeco 2" />
+        </a>
+    </figure>
+
+    <figure id="3">
+        <a href="https://www.google.com/maps/place/Aguas+De+Villa+María/@-32.4131603,-63.2487457,17z/data=!3m1!4b1!4m6!3m5!1s0x95cc42e0e207eb43:0x56897eb7fb5b663a!8m2!3d-32.4131649!4d-63.2461708!16s%2Fg%2F11h75pt_yk?entry=ttu" target="_blank" rel="noopener noreferrer">
+            <img src="/images/romeco3.jpg" title="Local Romeco 3"></img>
+        </a>
+    </figure> 
+    
+    </section>
+  );
+};
--- romeco/src/pages/romeco.js
+++ romeco/src/pages/romeco.js
@@ -1,16 +1,15 @@
 import '../styles/romeco.css'
+import { GalleryRomeco } from '../components/galleryRomeco';
 export const About = () => {
     return (
         <section>
-            <div className='DivImagen'></div>
-            <div>
-                <h1>ROMECO</h1>
-                <p>Bienvenido a nuestra joyería, donde la elegancia y la excelencia se encuentran para crear momentos inolvidables.</p> 
-                <p>Nos enorgullece ofrecer una exquisita selección de joyas finas, meticulosamente diseñadas para realzar la belleza y la singularidad de cada individuo.</p>
-                <p>Nuestra pasión por la artesanía se refleja en cada pieza que exhibimos, desde deslumbrantes diamantes hasta delicadas perlas, pasando por vibrantes gemas de colores. Cada artículo es cuidadosamente seleccionado para garantizar su calidad y belleza duradera.</p>
-                <p>Nos esforzamos por brindar una experiencia de compra personalizada y sin igual. Nuestro equipo de expertos en joyería está aquí para ayudarte a encontrar la pieza perfecta que se adapte a tu estilo y ocasión especial.</p>
-                <p>Ya sea que estés buscando el anillo de compromiso ideal, un regalo significativo o simplemente deseas darte un capricho, te invitamos a explorar nuestra colección y descubrir el lujo atemporal que define a nuestra joyería.</p>
-            </div>
+            <h1>Romeco</h1>
+            <p>Bienvenido a nuestra joyería, donde la elegancia y la excelencia se encuentran para crear momentos inolvidables.</p> 
+            <p>Nos enorgullece ofrecer una exquisita selección de joyas finas, meticulosamente diseñadas para realzar la belleza y la singularidad de cada individuo.</p>
+            <p>Nuestra pasión por la artesanía se refleja en cada pieza que exhibimos, desde deslumbrantes diamantes hasta delicadas perlas, pasando por vibrantes gemas de colores. Cada artículo es cuidadosamente seleccionado para garantizar su calidad y belleza duradera.</p>
+            <p>Nos esforzamos por brindar una experiencia de compra personalizada y sin igual. Nuestro equipo de expertos en joyería está aquí para ayudarte a encontrar la pieza perfecta que se adapte a tu estilo y ocasión especial.</p>
+            <p>Ya sea que estés buscando el anillo de compromiso ideal, un regalo significativo o simplemente deseas darte un capricho, te invitamos a explorar nuestra colección y descubrir el lujo atemporal que define a nuestra joyería.</p>
+            <GalleryRomeco/> 
         </section>
       
     );
