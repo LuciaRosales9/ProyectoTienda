3b1be7968c497bd6463d29cfbc92430d34d1e3e2
AbrilConrero <abrilconrero@gmail.com>
2024-04-16 16:17:05 -0300

    Update galleryRomeco.js
    cambios

--- romeco/src/components/galleryRomeco.js
+++ romeco/src/components/galleryRomeco.js
@@ -1,4 +1,7 @@
 import '../styles/Gallery.css'
+import React from 'react';
+//import { Nombre } from 'ruta'; // Para importar elementos específicos (no predeterminados) exportados desde el archivo en la ruta especificada
+
 
 export const GalleryRomeco = () => {
   return (
@@ -17,7 +20,7 @@ export const GalleryRomeco = () => {
 
     <figure id="3">
         <a href="https://www.google.com/maps/place/Aguas+De+Villa+María/@-32.4131603,-63.2487457,17z/data=!3m1!4b1!4m6!3m5!1s0x95cc42e0e207eb43:0x56897eb7fb5b663a!8m2!3d-32.4131649!4d-63.2461708!16s%2Fg%2F11h75pt_yk?entry=ttu" target="_blank" rel="noopener noreferrer">
-            <img src="/images/romeco3.jpg" title="Local Romeco 3"></img>
+            <img src="/images/romeco3.jpg" title="Local Romeco 3" ></img>
         </a>
     </figure> 
     
